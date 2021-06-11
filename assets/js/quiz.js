adae8b96cceec51243e673a29776e22672fec1c6
TylerLadas <tylerladas@hotmail.com>
2021-06-11 14:28:05 -0400

    reformat HTML and update CSS

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,61 +1,62 @@
-var timer = document.getElementById("timer");
-var startScreen = document.getElementById("startScreen");
-var start = document.getElementById("start-button");
-
-var message = ""
-
+const timer = document.getElementById("timer");
+const startScreen = document.getElementById("startScreen");
+const startButton = document.getElementById("startButton");
+const quizScreen = document.getElementById("quizScreen");
+var questionHeader = document.getElementById("questionHeader");
+let message = ""
+let questionCount = 0;
 // creates questions and answers //
 
 var questions = [
     {
         question: "Commonly used data types DO Not Include:",
-        answers: {
-            1: "strings",
-            2: "booleans",
-            3: "alerts",
-            4: "numbers"
-        },
-        correctAnswer: 3
+        answers: [
+            "1: strings",
+            "2: booleans",
+            "3: alerts",
+            "4:numbers"
+        ],
+        correctAnswer: "3"
     },
     {
         question: "The condition in an if/else statement is encloded with _________.",
-        answers: {
-            1: "quotes",
-            2: "curly brackets",
-            3: "parenthesis",
-            4: "square brackets"
-        },
-        correctAnswer: 3
+        answers: [
+            "1: quotes",
+            "2: curly brackets",
+            "3: parenthesis",
+            "4: square brackets"
+        ],
+        correctAnswer: "3"
     },
     {
         question: "Arrays in JavaScript can be used to store ________.",
-        answers: {
-            1: "numbers and strings",
-            2: "other arrays",
-            3: "booleans",
-            4: "all of the above"
-        },
-        correctAnswer: 4
+        answers: [
+            "1: numbers and strings",
+            "2: other arrays",
+            "3: booleans",
+            "4: all of the above"
+        ],
+        correctAnswer: "4"
     },
     {
         question: "String values must be enclosed with ________ when being assigned to variables.",
-        answers: {
-            1: "commas",
-            2: "curly brackets",
-            3: "quotes",
-            4: "parenthesis"
-        },
-        correctAnswer: 3
+        answers: [
+            "1: commas",
+            "2: curly brackets",
+            "3: quotes",
+            "4: parenthesis"
+        ],
+        correctAnswer: "3"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is",
-        answers: {
-            1: "JavaScript",
-            2: "terminal/bash",
-            3: "for loops",
-            4: "console.log"
-        },
-        correctAnswer: 4
+        answers: [
+            "1: JavaScript",
+            "2: terminal/bash",
+            "3: for loops",
+            "4: console.log"
+        ],
+        correctAnswer: "4"
     },
 ];
 
@@ -75,48 +76,75 @@ function countdown() {
     }, 1000);
 };
 
-// add quizbox//
+// add quizbox html elements/
 function quizbox() {
     var quizDiv = document.createElement("div");
     quizDiv.className = "quizbox";
     document.body.appendChild(quizDiv);
 
-    var quizH2 = document.createElement("h2");
-    quizH2.innerText = "question";
-    quizDiv.appendChild(quizH2);
-
-    var buttonDiv = document.createElement("div");
-    buttonDiv.className = "buttonDiv";
-    quizDiv.appendChild(buttonDiv);
+    
     
     var button1 = document.createElement("button");
-    button1.innerText = "b1";
+    button1.innerText = "";
     buttonDiv.appendChild(button1);
 
     var button2 = document.createElement("button");
-    button2.innerText = "b2";
+    button2.innerText = "";
     buttonDiv.appendChild(button2);
 
     var button3 = document.createElement("button");
-    button3.innerText = "b3";
+    button3.innerText = "";
     buttonDiv.appendChild(button3);
 
     var button4 = document.createElement("button");
-    button4.innerText = "b4";
+    button4.innerText = "";
     buttonDiv.appendChild(button4);
 
     var result = document.createElement("p");
     result.innerText = "result";
-    quizDiv.appendChild(result);
-}
+    quizDiv.appendChild(result);        
+    
+    // initial question selection //
+    quizH2.innerText = questions[questionCount].question;
+    button1.innerText = questions[questionCount].answers[0];
+    button2.innerText = questions[questionCount].answers[1];
+    button3.innerText = questions[questionCount].answers[2];
+    button4.innerText = questions[questionCount].answers[3];
 
+    button1.onclick = choose0;
+    button2.onclick = choose1;
+    button3.onclick = choose2;
+    button4.onclick = choose3;
+
+// check for correct answer //
+function checkAnswer(answer) {
+    if (questions[questionCount].correctAnswer === questions[questionCount].choices[answer]) {
+
+    } else {
+        timer -= 10;
+    }
+};
+
+function choose0 () {
+    checkAnswer(0);
+}
+function choose1  () {
+    checkAnswer(1);
+}
+function choose2 () {
+    checkAnswer(2);
+}
+function choose3 () {
+    checkAnswer(3);
+}
+}
 // starts quiz //
 function startQuiz() {
     countdown();
     startScreen.remove();
-    start.remove();
+    startButton.remove();
     quizbox();   
 }
 
 
-start.onclick = startQuiz
+startButton.onclick = startQuiz;
