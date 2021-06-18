b903891849a46e57bb2d53d21ffcf85af41c2566
TylerLadas <tylerladas@hotmail.com>
2021-06-18 10:22:03 -0400

    fix high score bug #2

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -196,6 +196,7 @@ headerScore.addEventListener("click", function() {
     startScreen.style.display = "none";
     scoreDisplay.style.display = "block";
     gameOver.style.display = "none";
+    quizScreen.style.display = "none";
     var savedScores = localStorage.getItem("high score");
 
     if (savedScores === null) {
